0e0863f66dabef2fe10efa5affebfea0e59a5871
Iladcsoft <ila.dcsoft@gmail.com>
2015-08-25 18:40:22 +0530

    Commit through GitHub.com
    Same constant changes

--- src/com/cs/ne/dao/Constants.java
+++ src/com/cs/ne/dao/Constants.java
@@ -5,4 +5,6 @@ public class Constants {
 	public static final String getIdDetails="select id from data";
 	public static final String getStudentDetails="select name,phone,mail from data where id=?";
 
+
+//New Change inside Github.com Repository
 }
